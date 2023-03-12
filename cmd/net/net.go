e0a83b783fc5c88d2a914175e5a2b5e943a6053a
Rafael Pissolatto Nunes <rafaelpissolatto@hotmail.com>
2023-03-12 13:05:17 -0300

    feat: initial commit of go-toolbox

---
+++ cmd/net/net.go
@@ -0,0 +1,21 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package net
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// netCmd represents the net command
+var NetCmd = &cobra.Command{
+	Use:   "net",
+	Short: "Net is a pallet of network tools commands",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+func init() {
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import "github.com/rafaelpissolatto/go-toolbox/cmd"
+
+func main() {
+	cmd.Execute()
+}
